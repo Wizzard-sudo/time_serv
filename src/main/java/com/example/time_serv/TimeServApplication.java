cccdcba90cc9fc3e73f20cadfaab2d266ec74b9f
Wizzard-sudo <kolia.makarevich.98@gmail.com>
2021-02-05 17:10:51 +0300

    work with application.properties, profiles and yaml

--- src/main/java/com/example/time_serv/TimeServApplication.java
+++ src/main/java/com/example/time_serv/TimeServApplication.java
@@ -1,16 +1,18 @@
 package com.example.time_serv;
 
+import com.example.time_serv.times.TimeProviderProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:custom.properties")
+@EnableConfigurationProperties(TimeProviderProperties.class)
 public class TimeServApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TimeServApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TimeServApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/time_serv/times/TimeService.java
+++ src/main/java/com/example/time_serv/times/TimeService.java
@@ -1,5 +1,6 @@
 package com.example.time_serv.times;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
@@ -9,8 +10,16 @@ import java.util.logging.Logger;
 @Service
 public class TimeService {
 
+    private final TimeProviderProperties timeProviderProperties;
+
+    @Autowired
+    public TimeService(TimeProviderProperties timeProviderProperties) {
+        this.timeProviderProperties = timeProviderProperties;
+    }
+
     public void printCurrentTime() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat(timeProviderProperties.getFormat());
+        Logger.getLogger(TimeService.class.getName()).info(timeProviderProperties.getDescription());
         Logger.getLogger(TimeService.class.getName()).info("current time: " + dateFormat.format(new Date()));
     }
 }
